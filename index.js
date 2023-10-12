8063f3d946ffc0d8c4668c2aef3cfdd353f196b6
Njugunasam <sammynjuguna5826@gmail.com>
2023-10-12 14:00:04 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+
+  
+
+//define function superbowlwin
+function superbowlWin(array) {
+    const winningYearObj = array.find(obj => obj.result === "W");
+    //put conditional statement
+    if (winningYearObj) {
+        //return the winning year
+      return winningYearObj.year;
+    } else {
+      return "undefined";
+    }
+  }
+  //put an array
+  const superBowlResults = [
+    { year: "2021", result: "W" },
+    { year: "2022", result: "undefined" },
+    { year: "2023", result: "W" },
+  ];
+  
+  const winningYear = superbowlWin(superBowlResults);
+  console.log(winningYear);
